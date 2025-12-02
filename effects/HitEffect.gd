c634683f31b19c8ae5e053f6463ade958166e8e8
trueblue222 <shinieechul2012@naver.com>
2025-12-02 11:40:08 +0900

    stage2 jungle 에셋 추가

--- effects/HitEffect.gd
+++ effects/HitEffect.gd
@@ -1,6 +1,6 @@
 extends GPUParticles2D
 
 func _ready():
-    emitting = true
-    await get_tree().create_timer(lifetime + 0.2).timeout
-    queue_free()
+	emitting = true
+	await get_tree().create_timer(lifetime + 0.2).timeout
+	queue_free()
