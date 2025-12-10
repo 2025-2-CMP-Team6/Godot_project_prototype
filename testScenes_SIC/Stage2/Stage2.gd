075be25a2b3b11064d1d8bc2b7fa0f98a1bca9e4
trueblue222 <shinieechul2012@naver.com>
2025-12-10 13:07:58 +0900

    stage4 에셋 추가, stage3.gd 생성 후 카메라 줌인 테스트 중

--- testScenes_SIC/Stage2/Stage2.gd
+++ testScenes_SIC/Stage2/Stage2.gd
@@ -7,4 +7,4 @@ func _on_portal_body_entered(body):
 	if body.is_in_group("player"):
 		print("플레이어가 포탈에 진입했습니다!")
 		# 여기에 포탈 이동 로직을 추가하세요
-		get_tree().change_scene_to_file("res://testScenes_SIC/Stage3/Stage3.tscn")
+		SceneTransition.fade_to_scene("res://testScenes_SIC/Stage3/Stage3.tscn")
---
+++ testScenes_SIC/Stage3/Stage3.gd
@@ -0,0 +1,4 @@
+extends World
+
+func _ready():
+	await camera_intro_effect(Vector2(0.5, 0.6))
